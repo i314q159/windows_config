219364465d52529164c754fd0b3fb250241307d1
i314q159 <i314q159@outlook.com>
2025-08-21 08:12:43 +0800

    Update winget settings and init script
    Replaces winget.json with settings.json for configuration, adds proxy and info commands to init.ps1, and enables ProxyCommandLineOptions. This streamlines settings management and improves script functionality.

--- winget/init.ps1
+++ winget/init.ps1
@@ -1,2 +1,7 @@
+winget settings --enable ProxyCommandLineOptions
+# winget install git.git --proxy 127.0.0.1:12334
+
 winget settings set DefaultProxy http://127.0.0.1:12334
 winget settings reset DefaultProxy
+
+winget --info
