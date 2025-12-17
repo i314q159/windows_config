5b48648e3f738346b4b22d3559974e8fcd91cf80
i314q159 <i314q159@outlook.com>
2025-12-17 13:26:14 +0800

    Update source.ps1

--- winget/source.ps1
+++ winget/source.ps1
@@ -1,5 +1,8 @@
 winget source rm msstore
-winget source add msstore https://storeedgefd.dsx.mp.microsoft.com/v9.0 
+winget source add msstore https://storeedgefd.dsx.mp.microsoft.com/v9.0
 
 winget source rm winget
 winget source add winget https://cdn.winget.microsoft.com/cache
+
+winget source rm winget-front
+winget source add winget-font https://cdn.winget.microsoft.com/fonts
