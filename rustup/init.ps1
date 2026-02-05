ba85bee1b5b3f807f90c77dff58899fe04f79cea
i314q159 <i314q159@outlook.com>
2026-02-05 19:56:45 +0800

    rm msvc

--- rustup/init.ps1
+++ rustup/init.ps1
@@ -5,6 +5,6 @@ setx RUSTUP_DIST_SERVER https://rsproxy.cn
 setx RUSTUP_UPDATE_ROOT https://rsproxy.cn/rustup
 
 rustup toolchain install stable-x86_64-pc-windows-gnu
-rustup default stable-x86_64-pc-windows-msvc 
+rustup default stable-x86_64-pc-windows-gnu
 
 cp config.toml ~/.cargo/config.toml
