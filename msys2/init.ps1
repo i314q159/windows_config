b838dd5b7a5b62b2a6a8213fa75a4126c11a9458
i314q159 <i314q159@outlook.com>
2026-02-05 13:06:59 +0800

    Update init.ps1

--- msys2/init.ps1
+++ msys2/init.ps1
@@ -1,1 +1,1 @@
-pacman -Sy mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-make mingw-w64-x86_64-cmake
+pacman -Sy mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-cmake mingw-w64-x86_64-make
