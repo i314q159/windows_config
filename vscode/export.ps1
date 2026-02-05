701c0b6c709f6cbae2df938812ebe14f59a824ab
i314q159 <i314q159@outlook.com>
2026-02-05 15:08:29 +0800

    update vscode config

--- vscode/export.ps1
+++ vscode/export.ps1
@@ -1,2 +1,2 @@
-cp C:\Users\i314q159\AppData\Roaming\Code\User\settings.json settings.json
+cp ~\AppData\Roaming\Code\User\settings.json settings.json
 code --list-extensions > extensions.txt 
--- vscode/import.ps1
+++ vscode/import.ps1
@@ -1,4 +1,4 @@
-cp settings.json C:\Users\i314q159\AppData\Roaming\Code\User\settings.json
+cp settings.json ~\Users\i314q159\AppData\Roaming\Code\User\settings.json
 foreach ($extension in Get-Content extensions.txt) {
     code --install-extension $extension --force 
 }
