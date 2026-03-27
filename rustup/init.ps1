3832dddfd23f09e5bcde066676e16ae7cf5ca537
i314q159 <i314q159@outlook.com>
2026-03-28 00:14:49 +0800

    update

--- rustup/init.ps1
+++ rustup/init.ps1
@@ -4,9 +4,10 @@ setx RUSTUP_UPDATE_ROOT https://mirrors.cernet.edu.cn/rustup/rustup
 # setx RUSTUP_DIST_SERVER https://rsproxy.cn
 # setx RUSTUP_UPDATE_ROOT https://rsproxy.cn/rustup
 
-rustup toolchain install stable-x86_64-pc-windows-gnu
-rustup default stable-x86_64-pc-windows-gnu
+# rustup toolchain install stable-x86_64-pc-windows-gnu
+# rustup default stable-x86_64-pc-windows-gnu
 
-rustup toolchain uninstall stable-x86_64-pc-windows-msvc
+rustup toolchain install stable-x86_64-pc-windows-msvc
+rustup default stable-x86_64-pc-windows-msvc
 
 cp config.toml ~/.cargo/config.toml
