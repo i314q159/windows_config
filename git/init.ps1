aa40961d64cbb84c6a7cc74fb176b7351d9b7c72
i314q159 <i314q159@outlook.com>
2026-06-22 19:22:14 +0800

    update

---
+++ git/init.ps1
@@ -0,0 +1,14 @@
+git config --global core.editor code
+git config --global --add safe.directory '*'
+git config --global init.defaultBranch main
+git config --global core.autocrlf true
+
+git config --global user.name i314q159
+git config --global user.email i314q159@outlook.com
+
+git config --global user.name
+git config --global user.email
+
+# proxy
+# git config --global http.proxy "http://127.0.0.1:12334"
+# git config --global https.proxy "http://127.0.0.1:12334"
