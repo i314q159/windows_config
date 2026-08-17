e54eed1ae0b1088a3e51493f3ca0bc3dffa046d9
i314q159 <i314q159@outlook.com>
2026-08-17 13:13:13 +0800

    update

--- git/init.ps1
+++ git/init.ps1
@@ -1,7 +1,7 @@
 git config --global core.editor code
 git config --global --add safe.directory '*'
 git config --global init.defaultBranch main
-git config --global core.autocrlf true
+git config --global core.autocrlf false
 
 git config --global user.name i314q159
 git config --global user.email i314q159@outlook.com
